cfd3f465bf68ce3781035742fadf039a798f33a2
michelenatale <104991886+michelenatale@users.noreply.github.com>
2024-05-28 20:31:24 +0200

    Update

---
+++ Coprimes/TestCoprimes/CoprimesRandom.cs
@@ -0,0 +1,277 @@
+﻿
+
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace TestCoprimes;
+
+
+public class CoprimesRandom
+{
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public T[] RngRangeCoprime<T>(int size) where T : INumber<T>
+  {
+    if(typeof(T) == typeof(BigInteger))
+     throw new NotImplementedException();
+
+    var result = new T[size]; 
+    this.NextCoprimesValue(result);
+    return result;
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static BigInteger[] RngRangeCoprimeBigInteger(int size, int bits) 
+  {
+    var result = new BigInteger[size];
+      NextCoprimesValueBigInteger(result, bits);
+    return result;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public T NextRangeCoprime<T>() where T : INumber<T>
+  {
+    if(typeof(T) == typeof(BigInteger))
+      throw new NotImplementedException();
+    return this.NextCoprimesValue<T>();
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public void NextRangeCoprime<T>(T[] destination) where T : INumber<T>
+  {
+    this.NextCoprimesValue(destination);
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private unsafe T NextCoprimesValue<T>() where T : INumber<T>
+  {
+    var coprimes = CoprimesSource.ToCoprimes256();
+    TypeCode typecode = Type.GetTypeCode(typeof(T)); 
+
+    var rbyte = new byte[sizeof(T)];
+    var cplengthg1 = coprimes.Length - 2;
+    RandomHolder.ToBytes(rbyte, (byte)coprimes.Length);
+    switch (typecode)
+    {
+      case TypeCode.SByte:
+        return T.CreateTruncating((coprimes[(rbyte[0] % cplengthg1) + 2]) & sbyte.MaxValue);
+      case TypeCode.Byte:
+        return T.CreateTruncating(coprimes[(rbyte[0] % cplengthg1) + 2]);
+      case TypeCode.Int16:
+        var rshort = new ushort[1];
+        rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+        rbyte[1] = coprimes[rbyte[1]];
+        Buffer.BlockCopy(rbyte, 0, rshort, 0, rbyte.Length);
+        return T.CreateTruncating(rshort[0] & short.MaxValue);
+      case TypeCode.UInt16:
+        var rushort = new ushort[1];
+        rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+        rbyte[1] = coprimes[rbyte[1]];
+        Buffer.BlockCopy(rbyte, 0, rushort, 0, rbyte.Length);
+        return T.CreateTruncating(rushort[0]);
+      case TypeCode.Int32:
+        var rint32 = new uint[1];
+        rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+        for (var i = 1; i < rbyte.Length; i++)
+          rbyte[i] = coprimes[rbyte[i]];
+        Buffer.BlockCopy(rbyte, 0, rint32, 0, rbyte.Length);
+        return T.CreateTruncating(rint32[0] & int.MaxValue);
+      case TypeCode.UInt32:
+        var ruint32 = new uint[1];
+        rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+        for (var i = 1; i < rbyte.Length; i++)
+          rbyte[i] = coprimes[rbyte[i]];
+        Buffer.BlockCopy(rbyte, 0, ruint32, 0, rbyte.Length);
+        return T.CreateTruncating(ruint32[0]);
+      case TypeCode.Int64:
+        var rint64 = new ulong[1];
+        rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+        for (var i = 1; i < rbyte.Length; i++)
+          rbyte[i] = coprimes[rbyte[i]];
+        Buffer.BlockCopy(rbyte, 0, rint64, 0, rbyte.Length);
+        return T.CreateTruncating(rint64[0] & long.MaxValue);
+      case TypeCode.UInt64:
+        var ruint64 = new ulong[1];
+        rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+        for (var i = 1; i < rbyte.Length; i++)
+          rbyte[i] = coprimes[rbyte[i]];
+        Buffer.BlockCopy(rbyte, 0, ruint64, 0, rbyte.Length);
+        return T.CreateTruncating(ruint64[0]);
+      default:
+        if (typeof(T) == typeof(Int128))
+        {
+          Span<UInt128> rint128;
+          rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+          for (var i = 1; i < rbyte.Length; i++)
+            rbyte[i] = coprimes[rbyte[i]];
+          fixed (byte* ptr = rbyte) rint128 = new Span<UInt128>(ptr, 1);
+          return T.CreateTruncating(rint128[0] & (UInt128)Int128.MaxValue);
+        }
+        else if (typeof(T) == typeof(UInt128))
+        {
+          Span<UInt128> ruint128;
+          rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+          for (var i = 1; i < rbyte.Length; i++)
+            rbyte[i] = coprimes[rbyte[i]];
+          fixed (byte* ptr = rbyte) ruint128 = new Span<UInt128>(ptr, 1);
+          return T.CreateTruncating(ruint128[0]);
+        }
+        else break;
+    }
+    throw new TypeLoadException();
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private unsafe void NextCoprimesValue<T>(T[] destination) where T : INumber<T>
+  {
+    if (typeof(T) == typeof(BigInteger))
+      throw new NotImplementedException();
+
+    var coprimes = CoprimesSource.ToCoprimes256();
+    var typecode = Type.GetTypeCode(typeof(T));
+
+    var tsize = sizeof(T);
+    var cplengthg1 = coprimes.Length - 2;
+    var rbyte = new byte[tsize * destination.Length];
+    RandomHolder.ToBytes(rbyte, (byte)coprimes.Length);
+
+    for (var i = 0; i < rbyte.Length; i++)
+      if (i % tsize == 0) rbyte[i] = coprimes[(rbyte[i] % cplengthg1) + 2];
+      else rbyte[i] = coprimes[rbyte[i]];
+
+    switch (typecode)
+    {
+      case TypeCode.SByte:
+        for (var i = 0; i < destination.Length; i++)
+          destination[i] = T.CreateTruncating(rbyte[i] & sbyte.MaxValue);
+        return;
+      case TypeCode.Byte:
+        for (var i = 0; i < destination.Length; i++)
+          destination[i] = T.CreateTruncating(rbyte[i]);
+        return;
+      case TypeCode.Int16:
+        var rshort = new ushort[1];
+        for (var i = 0; i < destination.Length; i++)
+        {
+          Buffer.BlockCopy(rbyte, i * tsize, rshort, 0, tsize);
+          destination[i] = T.CreateTruncating(rshort[0] & short.MaxValue);
+        }
+        return;
+      case TypeCode.UInt16:
+        var rushort = new ushort[1];
+        for (var i = 0; i < destination.Length; i++)
+        {
+          Buffer.BlockCopy(rbyte, i * tsize, rushort, 0, tsize);
+          destination[i] = T.CreateTruncating(rushort[0]);
+        }
+        return;
+      case TypeCode.Int32:
+        var rint32 = new uint[1];
+        for (var i = 0; i < destination.Length; i++)
+        {
+          Buffer.BlockCopy(rbyte, i * tsize, rint32, 0, tsize);
+          destination[i] = T.CreateTruncating(rint32[0] & int.MaxValue);
+        }
+        return;
+      case TypeCode.UInt32:
+        var ruint32 = new uint[1];
+        for (var i = 0; i < destination.Length; i++)
+        {
+          Buffer.BlockCopy(rbyte, i * tsize, ruint32, 0, tsize);
+          destination[i] = T.CreateTruncating(ruint32[0]);
+        }
+        return;
+      case TypeCode.Int64:
+        var rint64 = new ulong[1];
+        for (var i = 0; i < destination.Length; i++)
+        {
+          Buffer.BlockCopy(rbyte, i * tsize, rint64, 0, tsize);
+          destination[i] = T.CreateTruncating(rint64[0] & long.MaxValue);
+        }
+        return;
+      case TypeCode.UInt64:
+        var ruint64 = new ulong[1];
+        for (var i = 0; i < destination.Length; i++)
+        {
+          Buffer.BlockCopy(rbyte, i * tsize, ruint64, 0, tsize);
+          destination[i] = T.CreateTruncating(ruint64[0]);
+        }
+        return;
+      default:
+        if (typeof(T) == typeof(Int128))
+        {
+          Span<UInt128> rint128;
+          fixed (byte* ptr = rbyte) rint128 = new Span<UInt128>(ptr, destination.Length);
+          for (var i = 0; i < destination.Length; i++)
+            destination[i] = T.CreateTruncating(rint128[i] & (UInt128)Int128.MaxValue);
+          return;
+        }
+        else if (typeof(T) == typeof(UInt128))
+        {
+          Span<UInt128> ruint128;
+          fixed (byte* ptr = rbyte) ruint128 = new Span<UInt128>(ptr, destination.Length);
+          for (var i = 0; i < destination.Length; i++)
+            destination[i] = T.CreateTruncating(ruint128[i] & (UInt128)Int128.MaxValue);
+          return;
+        }
+        else break;
+    }
+    throw new TypeLoadException();
+  }
+
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static void NextRangeCoprimeBigInteger(
+    BigInteger[] destination, int bits)
+  {
+    NextCoprimesValueBigInteger(destination, bits);
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  public static BigInteger NextRangeCoprimeBigInteger(int bits)
+  {
+    if (!int.IsPow2(bits))
+      throw new NotImplementedException();
+
+    var coprimes = CoprimesSource.ToCoprimes256();
+
+    var rbyte = new byte[bits / 8];
+    var cplengthg1 = coprimes.Length - 2;
+    RandomHolder.ToBytes(rbyte, (byte)coprimes.Length);
+
+    rbyte[0] = coprimes[(rbyte[0] % cplengthg1) + 2];
+    for (var i = 1; i < rbyte.Length; i++)
+      rbyte[i] = coprimes[rbyte[i]];
+    return new BigInteger(rbyte, true, false);
+  }
+
+  private static void NextCoprimesValueBigInteger(BigInteger[] destination, int bits)
+  {
+    if (!int.IsPow2(bits))
+      throw new NotImplementedException();
+
+    var coprimes = CoprimesSource.ToCoprimes256();
+
+    var tsize = bits / 8;
+    var cplengthg1 = coprimes.Length - 2;
+    var rbyte = new byte[tsize * destination.Length];
+    RandomHolder.ToBytes(rbyte, (byte)coprimes.Length);
+
+    for (var i = 0; i < rbyte.Length; i++)
+      if (i % tsize == 0) rbyte[i] = coprimes[(rbyte[i] % cplengthg1) + 2];
+      else rbyte[i] = coprimes[rbyte[i]];
+
+    for (var i = 0; i < destination.Length; i++)
+    {
+      var b = rbyte.Skip(i * tsize).Take(tsize).ToArray();
+      destination[i] = new BigInteger(b, true, false);
+    }
+
+  }
+}
---
+++ Coprimes/TestCoprimes/Program.cs
@@ -0,0 +1,333 @@
+﻿
+
+using TestCoprimes;
+using System.Numerics;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+
+namespace CoPrimesTests;
+
+
+public class Program
+{
+  public static void Main()
+  {
+    var rounds = 1000;
+    TestRangeCoprimes(rounds);
+
+
+    Console.WriteLine();
+    Console.WriteLine("FINISH");
+    Console.ReadLine();
+  }
+
+  private static void TestRangeCoprimes(int round)
+  {
+
+    Console.Write($"{nameof(TestRangeCoprimes)}: ");
+
+    var sw = new Stopwatch();
+
+    for (var i = 0; i < round; i++)
+    {
+
+      var cpr = new CoprimesRandom();
+
+      sw.Start();
+
+      var b = cpr.NextRangeCoprime<byte>();
+      if (!IsCoprimeRange(b))
+        throw new ArgumentException(null);
+
+      var bytes = cpr.RngRangeCoprime<byte>(128);
+      if (!IsCoprimeRange(bytes))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(bytes);
+      if (!IsCoprimeRange(bytes))
+        throw new ArgumentException(null);
+
+
+      var sb = cpr.NextRangeCoprime<sbyte>();
+      if (!IsCoprimeRange(sb))
+        throw new ArgumentException(null);
+
+      var sbytes = cpr.RngRangeCoprime<sbyte>(128);
+      if (!IsCoprimeRange(sbytes))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(sbytes);
+      if (!IsCoprimeRange(sbytes))
+        throw new ArgumentException(null);
+
+
+      var s = cpr.NextRangeCoprime<short>();
+      if (!IsCoprimeRange(s))
+        throw new ArgumentException(null);
+
+      var sht = cpr.RngRangeCoprime<short>(128);
+      if (!IsCoprimeRange(sht))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(sht);
+      if (!IsCoprimeRange(sht))
+        throw new ArgumentException(null);
+
+
+      var us = cpr.NextRangeCoprime<ushort>();
+      if (!IsCoprimeRange(us))
+        throw new ArgumentException(null);
+
+      var usht = cpr.RngRangeCoprime<ushort>(128);
+      if (!IsCoprimeRange(usht))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(usht);
+      if (!IsCoprimeRange(usht))
+        throw new ArgumentException(null);
+
+
+      var i32 = cpr.NextRangeCoprime<int>();
+      if (!IsCoprimeRange(i32))
+        throw new ArgumentException(null);
+
+      var i32s = cpr.RngRangeCoprime<int>(128);
+      if (!IsCoprimeRange(i32s))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(i32s);
+      if (!IsCoprimeRange(i32s))
+        throw new ArgumentException(null);
+
+
+      var ui32 = cpr.NextRangeCoprime<uint>();
+      if (!IsCoprimeRange(ui32))
+        throw new ArgumentException(null);
+
+      var ui32s = cpr.RngRangeCoprime<uint>(128);
+      if (!IsCoprimeRange(ui32s))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(ui32s);
+      if (!IsCoprimeRange(ui32s))
+        throw new ArgumentException(null);
+
+
+      var lng = cpr.NextRangeCoprime<long>();
+      if (!IsCoprimeRange(lng))
+        throw new ArgumentException(null);
+
+      var lngs = cpr.RngRangeCoprime<long>(128);
+      if (!IsCoprimeRange(lngs))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(lngs);
+      if (!IsCoprimeRange(lngs))
+        throw new ArgumentException(null);
+
+
+      var ulng = cpr.NextRangeCoprime<ulong>();
+      if (!IsCoprimeRange(ulng))
+        throw new ArgumentException(null);
+
+      var ulngs = cpr.RngRangeCoprime<ulong>(128);
+      if (!IsCoprimeRange(ulngs))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(ulngs);
+      if (!IsCoprimeRange(ulngs))
+        throw new ArgumentException(null);
+
+
+      var i128 = cpr.NextRangeCoprime<Int128>();
+      if (!IsCoprimeRange(i128))
+        throw new ArgumentException(null);
+
+      var i128s = cpr.RngRangeCoprime<Int128>(128);
+      if (!IsCoprimeRange(i128s))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(i128s);
+      if (!IsCoprimeRange(i128s))
+        throw new ArgumentException(null);
+
+
+      var ui128 = cpr.NextRangeCoprime<UInt128>();
+      if (!IsCoprimeRange(ui128))
+        throw new ArgumentException(null);
+
+      var ui128s = cpr.RngRangeCoprime<UInt128>(128);
+      if (!IsCoprimeRange(ui128s))
+        throw new ArgumentException(null);
+
+      cpr.NextRangeCoprime(ui128s);
+      if (!IsCoprimeRange(ui128s))
+        throw new ArgumentException(null);
+
+
+      //BigInteger
+      var bits = ToPowerTwo(RandomHolder.NextInt32(8, 1025));
+      
+      var bi = CoprimesRandom.NextRangeCoprimeBigInteger(bits);
+      if (!IsCoprimeRangeBigInteger(bi) || !IsLessEqualsBits(bi, bits))
+        throw new ArgumentException(null);
+
+      var bis = CoprimesRandom.RngRangeCoprimeBigInteger(128, bits);
+      if (!IsCoprimeRangeBigInteger(bis) || !IsLessEqualsBits(bis, bits))
+        throw new ArgumentException(null);
+
+      CoprimesRandom.NextRangeCoprimeBigInteger(bis, bits);
+      if (!IsCoprimeRangeBigInteger(bis) || !IsLessEqualsBits(bis, bits))
+        throw new ArgumentException(null);
+
+      sw.Stop();
+
+      if (i % 100 == 0)
+        Console.Write(".");
+    }
+
+
+    Console.Write($" all TypeChecks; r = {round:N0}; t = {sw.ElapsedMilliseconds}ms; td = {sw.ElapsedMilliseconds / round}ms");
+    Console.WriteLine(); Console.WriteLine();
+
+    Console.WriteLine();
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private unsafe static bool IsCoprimeRange<T>(T[] destination) where T : INumber<T>
+  {
+    BigInteger vmax = BigInteger.One << ToRangeNumber<T>();
+    for (var i = 0; i < destination.Length; i++)
+      if (BigInteger.GreatestCommonDivisor(vmax, UInt128.CreateTruncating(destination[i])) != 1)
+        return false;
+    return true;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private unsafe static bool IsCoprimeRange<T>(T value) where T : INumber<T>
+  {
+    BigInteger vmax = BigInteger.One << ToRangeNumber<T>();
+    return BigInteger.GreatestCommonDivisor(vmax, UInt128.CreateTruncating(value)) == 1;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private unsafe static int ToRangeNumber<T>() where T : INumber<T>
+  {
+    var tsize = sizeof(T);
+
+    if (typeof(T) == typeof(Int128)) return tsize * 8 - 1;
+    else if (typeof(T) == typeof(UInt128)) return tsize * 8;
+    else if (typeof(T) == typeof(BigInteger)) return tsize * 8;
+
+    TypeCode typecode = Type.GetTypeCode(typeof(T));
+    return typecode switch
+    {
+      TypeCode.Byte => tsize * 8,
+      TypeCode.SByte => tsize * 8 - 1,
+      TypeCode.Int16 => tsize * 8 - 1,
+      TypeCode.UInt16 => tsize * 8,
+      TypeCode.Int32 => tsize * 8 - 1,
+      TypeCode.UInt32 => tsize * 8,
+      TypeCode.Int64 => tsize * 8 - 1,
+      TypeCode.UInt64 => tsize * 8,
+      _ => throw new TypeAccessException(),
+    };
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static bool IsCoprimeRangeBigInteger(BigInteger[] numbers)
+  {
+
+    for (var i = 0; i < numbers.Length; i++)
+    {
+      var bits = numbers[i].IsPowerOfTwo
+        ? (int)BigInteger.Log2(numbers[i])
+        : (int)BigInteger.Log2(numbers[i]) + 1;
+      if (bits < 8) bits = 8;
+      while (!int.IsPow2(bits)) bits++;
+      var vmax = BigInteger.One << bits;
+      if (BigInteger.GreatestCommonDivisor(vmax, numbers[i]) != 1)
+        return false;
+    }
+    return true;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static bool IsCoprimeRangeBigInteger(BigInteger value)
+  {
+    var bits = value.IsPowerOfTwo
+        ? (int)BigInteger.Log2(value)
+        : (int)BigInteger.Log2(value) + 1;
+    if (bits < 8) bits = 8;
+    while (!int.IsPow2(bits)) bits++;
+
+    BigInteger vmax = BigInteger.One << bits;
+    return BigInteger.GreatestCommonDivisor(vmax, value) == 1;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static bool IsCoprimeRangeBigInteger(BigInteger value, int bits)
+  {
+    if (!int.IsPow2(bits)) throw new NotImplementedException();
+
+    BigInteger vmax = BigInteger.One << bits;
+    return BigInteger.GreatestCommonDivisor(vmax, value) == 1;
+  }
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static int ToPowerTwo(BigInteger value)
+  {
+    var bits = value.IsPowerOfTwo
+        ? (int)BigInteger.Log2(value)
+        : (int)BigInteger.Log2(value) + 1;
+    if (bits < 8) bits = 8;
+    return ToPowerTwo(bits);
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static int ToPowerTwo(int bits)
+  {
+    while (!int.IsPow2(bits)) bits++;
+    return bits;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static bool IsLessEqualsBits(BigInteger[] numbers, int bits)
+  {
+    for (int i = 0; i < numbers.Length; i++)
+      if (!IsLessEqualsBits(numbers[i], bits)) return false;
+    return true;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static bool IsEqualsBits(BigInteger number, int bits)
+  {
+    return ToPowerTwo(number) == bits;
+  }
+
+
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+  private static bool IsLessEqualsBits(BigInteger number, int bits)
+  {
+    return ToPowerTwo(number) <= bits;
+  }
+
+
+  //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+  //private static bool IsGreaterEqualsBits(BigInteger number, int bits)
+  //{
+  //  return ToPowerTwo(number) >= bits;
+  //}
+}
+
+
