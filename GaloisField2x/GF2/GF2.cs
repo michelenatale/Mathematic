8a407a906484f45a28c050a4f01630a7847123f8
michelenatale <104991886+michelenatale@users.noreply.github.com>
2025-03-12 01:56:58 +0100

    Update

---
+++ GaloisField2x/GF2/GF2.cs
@@ -0,0 +1,13 @@
+﻿
+namespace michele.natale.Numerics;
+
+
+/// <summary>
+/// Provides tools for Galois calculations (GF2), 
+/// where the character of the Galois field is always 2. 
+/// <para>Updated by <see href="https://github.com/michelenatale">© Michele Natale 2025</see></para>  
+/// </summary>
+public readonly partial struct GF2
+{
+
+}
